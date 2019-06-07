a7cdb7f88264515568978b386cb8473593b3a0cc
muammerturan <muammerturan@10.1.10.201>
2019-06-06 19:05:53 -0500

    TestNGChanges

--- Automation-Project1/src/test/java/automation/ActionPractice.java
+++ Automation-Project1/src/test/java/automation/ActionPractice.java
@@ -1,34 +1,31 @@
 package automation;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.util.Asserts;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class ActionPractice {
+public class ActionPractice extends TestBase {
 	
-	
-	WebDriver driver;
-	@Before
-	public void beforeM() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}
+
 	
 //	@Test
 //	public void test() {
@@ -91,37 +88,130 @@ public class ActionPractice {
 //		action.sendKeys(Keys.ARROW_UP).perform();
 //
 //	}
-
+//
+//	
+//
+//	//@Test
+//	public void test6() throws InterruptedException {
+//		
+//		driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+//		
+//		WebElement mainLocation=driver.findElement(By.id("draggable"));
+//		
+//		WebElement targetLocation=driver.findElement(By.id("droptarget"));
+//
+//		Actions action=new Actions(driver);
+//				
+//		//action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation).release().build().perform();
+//		
+//		action.dragAndDrop(mainLocation, targetLocation).perform();
+//		
+//		//action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation, 10, 150).release().build().perform();
+//		
+//		Thread.sleep(3000);
+//
+//	}
+//	
+//	
+//	//@Test
+//	public void test7() throws InterruptedException {
+//		
+//		driver.get("http://demo.guru99.com/test/drag_drop.html");
+//		
+//		WebElement mainLocation=driver.findElement(By.id("fourth"));
+//		
+//		WebElement targetLocation=driver.findElement(By.id("amt7"));
+//
+//		Actions action=new Actions(driver);
+//				
+//		//action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation).release().build().perform();
+//		
+//		action.dragAndDrop(mainLocation, targetLocation).perform();
+//		
+//		//action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation, 10, 150).release().build().perform();
+//		
+//		Thread.sleep(3000);
+//
+//	}
+//	
+//	//@Test
+//	public void test8() {
+//		
+//		driver.get("http://demo.guru99.com/test/drag_drop.html");
+//		driver.manage().window().maximize();
+//
+//		// Right click the button to launch right click menu options
+//		Actions action = new Actions(driver);
+//
+//		WebElement link = driver.findElement(By.cssSelector(".context-menu-one"));
+//		action.contextClick(link).perform();
+//		// Click on Edit link on the displayed menu options
+//		WebElement element = driver.findElement(By.cssSelector(".context-menu-icon-delete"));
+//		element.click();
+//		
+//		// Accept the alert displayed
+//		//driver.switchTo().alert().accept();
+//		// Closing the driver instance
+//		//driver.quit();
+//		
+//	}
+//	
 	
+	
+	    @Test
+		public void test9() {
+			
+			driver.get("http://toolsqa.com/iframe-practice-page/");
+			driver.manage().window().maximize();
+			
 
+			JavascriptExecutor exe = (JavascriptExecutor) driver;
+			 Integer numberOfFrames = Integer.parseInt(exe.executeScript("return window.length").toString());
+			 System.out.println("Number of iframes on the page are " + numberOfFrames);
+			
+			//By finding all the web elements using iframe tag
+			 List<WebElement> iframeElements = driver.findElements(By.tagName("iframe"));
+			 System.out.println("The total number of iframes are " + iframeElements.size());
+			 
+			
+			
+		}
+	
+	
+	
 	@Test
-	public void test6() throws InterruptedException {
+	public void test10() {
 		
-		driver.get("https://demos.telerik.com/kendo-ui/dragdrop/area");
+		driver.get("https://demos.telerik.com/kendo-ui/slider/angular");
 		
-		WebElement mainLocation=driver.findElement(By.id("draggable"));
 		
-		WebElement targetLocation=driver.findElement(By.xpath("//div[@class='test1']"));
-
 		Actions action=new Actions(driver);
-				
-		action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation).release().build().perform();
 		
-		//action.dragAndDrop(mainLocation, targetLocation).perform();
+//		WebElement slider=driver.findElement(By.xpath("(//div[@class='k-slider-track'])[1]"));
+	//	driver.switchTo().frame(0); 
 		
-		//action.moveToElement(mainLocation).clickAndHold().moveToElement(targetLocation, 10, 150).release().build().perform();
-		
-		Thread.sleep(3000);
+		WebElement slider=driver.findElement(By.xpath("(//div[@class='k-slider-track'])[2]"));
 
-	}
-	
-	
-	@After
-	public void tearDown() throws InterruptedException {
 		
-		Thread.sleep(3000);
-		//driver.quit();
+	
+			
+		Dimension dim=slider.getSize(); 
+		int y=dim.getHeight(); 
+		System.out.println(y);
+		
+		action.clickAndHold(slider).moveByOffset(9, -98).release().build().perform(); 
+		
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+
 	
 	
 	
--- Automation-Project1/src/test/java/automation/TestBase.java
+++ Automation-Project1/src/test/java/automation/TestBase.java
@@ -1,11 +1,13 @@
 package automation;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,12 +19,29 @@ public class TestBase {
 	public void setUp() {
 		
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver =new ChromeDriver(); 
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
 	
+	
+	public void switchWindow(String window) {
+		
+		Set <String> windows=driver.getWindowHandles(); 
+		
+	
+		for(String each:windows) {
+			
+			driver.switchTo().window(each); 
+			if(driver.getTitle().equals(window)) {
+				
+				break; 
+			}
+		}
+		
+			}
+		
 	
 	
 	@After
